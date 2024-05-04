cdbab5da119fd89d8baa5f65cbe59be829ef868c
zhoujunlin <zhoujunlin.work@outlook.com>
2024-05-04 13:26:43 +0800

    111111

---
+++ bak/25-todolist-编辑/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <ul class="todo-main">
+        <TodoItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"/>
+    </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: 'TodoList',
+    components: {TodoItem},
+    props: ['todos']
+}
+</script>
+
+<style>
+    /*list*/
+    .todo-main {
+      margin-left: 0px;
+      border: 1px solid #ddd;
+      border-radius: 2px;
+      padding: 0px;
+    }
+    .todo-empty {
+      height: 40px;
+      line-height: 40px;
+      border: 1px solid #ddd;
+      border-radius: 2px;
+      padding-left: 5px;
+      margin-top: 10px;
+    }
+    .todo-enter-active {
+      animation: atguigu 0.5s linear;
+    }
+
+    .todo-leave-active {
+      animation: atguigu 0.5s linear reverse;
+    }
+
+</style>
