73cd27e9271c7271b7774b6781c658a44f288d29
zhoujunlin <zhoujunlin.work@outlook.com>
2024-05-13 22:41:26 +0800

    路由守卫

---
+++ bak/15-路由/12-组件内守卫/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>我是Home内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item"  active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item"  active-class="active"  to="/home/message">Message</router-link  >
+        </li>
+      </ul>
+      <!-- 缓存组件 不写inclue属性 里面的组件都会缓存
+           include值 《组件名》
+           缓存多个用数组形式
+      -->
+      <!-- <keep-alive include="News"> -->
+      <keep-alive :include="['News']">
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
---
+++ bak/15-路由/12-组件内守卫/pages/News.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul>
+		<li :style="{opacity}">欢迎学习Vue</li>
+		<li>news001 <input type="text"/></li>
+		<li>news002 <input type="text"/></li>
+		<li>news003 <input type="text"/></li>
+	</ul>
+</template>
+
+<script>
+export default {
+    name: 'News',
+	data() {
+		return {
+			opacity: 1
+		}
+	},
+	// mounted(){
+	// 	this.timer = setInterval(() => {
+	// 		// 由于缓存了此组件  则路由切换后  这个定时器还是一直在执行。。。   不大好
+	// 		console.log("run");
+	// 		this.opacity -= 0.01
+	// 		if(this.opacity <= 0){
+	// 			this.opacity = 1
+	// 		}
+	// 	}, 16);
+	// },
+	// beforeDestroy(){
+	// 	clearInterval(this.timer)
+	// },
+	activated(){
+		this.timer = setInterval(() => {
+			// 路由切换出去后  定时器停止  路由激活后 定时器运行
+			console.log("run");
+			this.opacity -= 0.01
+			if(this.opacity <= 0){
+				this.opacity = 1
+			}
+		}, 16);
+	},
+	deactivated(){
+		clearInterval(this.timer)
+	}
+}
+</script>
+
+<style>
+
+</style>
